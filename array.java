6044fa1afd415db1addf6cc23c9a7fa75674d86d
Preethiselvam <47376744+Preethiselvam@users.noreply.github.com>
2019-03-09 16:28:51 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class array
+    {
+	public static void main (String[] args) 
+	{
+	Scanner ob=new Scanner(System.in);
+	int a=ob.nextInt();
+	int b=ob.nextInt();
+	int [][]m=new int [a][b];
+	int n=0,i,j;
+	for(i=1;i<=a;i++)
+	{
+		
+	}
+	for(j=1;j<=b;j++)
+	{
+		n=n+j;
+	}
+	System.out.println(+n);	
+	}
+	}
