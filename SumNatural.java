47bcba938ace1e368b6fa71fa29bcc90930f5166
Preethiselvam <47376744+Preethiselvam@users.noreply.github.com>
2019-03-09 10:59:36 +0530

    Create SumNatural.java

---
+++ SumNatural.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class SumNatural{
+	public static void main(String[] args){
+		int num=100,sum=0;
+		for(int i=1;i<=num;++i)
+		{
+			sum+=i;
+		}
+		System.out.println("Sum="+sum);
+	}
+}
