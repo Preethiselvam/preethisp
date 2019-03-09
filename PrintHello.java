241216b9ffb6cc6e60263e7c469064b2bde28009
Preethiselvam <47376744+Preethiselvam@users.noreply.github.com>
2019-03-09 10:23:38 +0530

    Create PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class PrintHello
+{
+public static void main(String args[])throws Exception
+{
+BufferedReader br=new BufferedReader(new InputstreamReader(new InputStreamReader(System.in));
+String line=br.readLine();
+int N=Integer.parseInt(line);
+for(int i=0;i<N,i++)
+System.out.println("hello");
+}
+}
+}
