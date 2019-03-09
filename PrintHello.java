65747091deb2048c7f074874b9a4cb5eeb3ff4cb
Preethiselvam <47376744+Preethiselvam@users.noreply.github.com>
2019-03-09 10:47:25 +0530

    Update PrintHello.java

--- PrintHello.java
+++ PrintHello.java
@@ -3,10 +3,10 @@ class PrintHello
 {
 public static void main(String args[])throws Exception
 {
-BufferedReader br=new BufferedReader(new InputstreamReader(new InputStreamReader(System.in));
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 String line=br.readLine();
 int N=Integer.parseInt(line);
-for(int i=0;i<N,i++)
+for(int i=0;i<N;i++){
 System.out.println("hello");
 }
 }
